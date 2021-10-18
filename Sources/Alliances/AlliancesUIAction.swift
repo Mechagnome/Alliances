399974d6853378a1312dc7588494b70b33b9d53d
linhey <is.linhey@outlook.com>
2021-10-18 18:34:12 +0800

    添加属性设置

--- Sources/Alliances/AlliancesUIAction.swift
+++ Sources/Alliances/AlliancesUIAction.swift
@@ -11,14 +11,17 @@ public class AlliancesUICore {
     
     let reload = PassthroughSubject<Void, Never>()
     
+    let canOpenSetting = CurrentValueSubject<Bool, Never>(true)
+    let canRun = CurrentValueSubject<Bool, Never>(true)
+
 }
 
 public protocol AlliancesDelegate {
     
     var core: AlliancesUICore { get }
     
-    var canOpenSetting: Bool { get }
-    var canRun: Bool { get }
+    var canOpenSetting: Bool { get set }
+    var canRun: Bool { get set }
 
     func openSetting()
     func run()
@@ -27,8 +30,15 @@ public protocol AlliancesDelegate {
 
 public extension AlliancesDelegate {
     
-    var canOpenSetting: Bool { true }
-    var canRun: Bool { true }
+    var canOpenSetting: Bool {
+        set { core.canOpenSetting.send(newValue) }
+        get { core.canOpenSetting.value }
+    }
+    
+    var canRun: Bool {
+        set { core.canRun.send(newValue) }
+        get { core.canRun.value }
+    }
     
     func reload() {
         core.reload.send()
