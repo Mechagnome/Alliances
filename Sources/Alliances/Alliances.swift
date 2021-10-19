65ab2ebcebc9c95c40dfa650767f4fca624bfdc7
linhey <is.linhey@outlook.com>
2021-10-19 10:55:43 +0800

    bundleID

--- Sources/Alliances/Alliances.swift
+++ Sources/Alliances/Alliances.swift
@@ -1,13 +1,14 @@
 import Foundation
 
 public protocol AlliancesApp: AlliancesDelegate {
+    /// 唯一标识
+    var bundleID: String { get }
     /// 标题
     var name: String { get }
     /// 备注
     var remark: String? { get }
     /// 子任务
     var tasks: [AlliancesApp] { get }
-
 }
 
 public extension AlliancesApp {
