e246b54ec5ccab326cf8dccf145c52d076ff9830
linhey <is.linhey@outlook.com>
2021-10-19 17:06:08 +0800

    添加 app 文件夹路径

--- Sources/Alliances/Alliances.swift
+++ Sources/Alliances/Alliances.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Stem
 
 public protocol AlliancesApp: AlliancesDelegate {
     
@@ -8,17 +9,24 @@ public protocol AlliancesApp: AlliancesDelegate {
     /// 备注
     var remark: String? { get }
     /// 子任务
-    var tasks: [AlliancesApp.Type] { get }
+    var tasks: [AlliancesApp] { get }
     /// 处理路由
     func deeplink(open url: URL) -> Bool
     
-    init()
+    var folder: FilePath.Folder { get }
+    
+    init(folder: FilePath.Folder)
 }
 
 
 public extension AlliancesApp {
     
+    func folder(task: AlliancesApp.Type) throws -> FilePath.Folder {
+       try folder.create(folder: task.bundleID)
+    }
+    
     var remark: String? { nil }
+    
     func deeplink(open url: URL) -> Bool { false }
     
 }
