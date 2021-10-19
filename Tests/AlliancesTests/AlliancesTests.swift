85ecbe3eec4021263d714e97f9b72b50893719fb
linhey <is.linhey@outlook.com>
2021-10-19 14:22:47 +0800

    TestApp

--- Tests/AlliancesTests/AlliancesTests.swift
+++ Tests/AlliancesTests/AlliancesTests.swift
@@ -2,6 +2,31 @@ import XCTest
 @testable import Alliances
 
 final class AlliancesTests: XCTestCase {
+    
+    class TestApp: AlliancesApp {
+        
+        var bundleID: String = UUID().uuidString
+        
+        var name: String { bundleID }
+        
+        var remark: String? {
+            "remark"
+        }
+        
+        var tasks: [AlliancesApp] = []
+        
+        var core: AlliancesUICore = .init()
+        
+        func openSettings() {
+            progress -= 0.05
+        }
+        
+        func run() throws {
+            progress += 0.05
+        }
+        
+    }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
